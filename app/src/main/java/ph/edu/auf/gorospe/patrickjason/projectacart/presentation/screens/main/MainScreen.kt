d3679decedace89718645d9e74d362a96904b645
Luis Rimando <alfonsorimando15@gmail.com>
2025-01-05 20:43:13 +0800

    Navigation

--- app/src/main/java/ph/edu/auf/gorospe/patrickjason/projectacart/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/ph/edu/auf/gorospe/patrickjason/projectacart/presentation/screens/main/MainScreen.kt
@@ -6,13 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
 import ph.edu.auf.gorospe.patrickjason.projectacart.presentation.screens.main.components.HistorySection
 import ph.edu.auf.gorospe.patrickjason.projectacart.presentation.screens.main.components.MapArea
 import ph.edu.auf.gorospe.patrickjason.projectacart.presentation.screens.main.components.SearchSection
 import ph.edu.auf.gorospe.patrickjason.projectacart.presentation.screens.main.components.TopBar
 
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavController) {
     Column(
         modifier = Modifier.fillMaxSize().background(Color(0xFFFFF7E8))
     ) {
